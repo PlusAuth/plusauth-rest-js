950eec55194d7be5356255b9bb59549d90be18db
Ismail H. Ayaz <ayazismailhakki@gmail.com>
2021-05-03 22:21:55 +0300

    docs: add missing documentation for custom domain and keys services

--- src/api/custom_domains.ts
+++ src/api/custom_domains.ts
@@ -3,29 +3,92 @@ import { IPagination, PaginatedResult } from '../interfaces';
 import { ICustomDomain } from '../interfaces/custom_domain';
 import { encodedQueryString } from '../utils';
 
+/**
+ * Service for interacting custom domains for your tenant.
+ */
 export class CustomDomainService extends HttpService {
   protected static prefix = '/custom-domain'
 
+  /**
+   * Retrieve or filter created custom domains.
+   *
+   * @param pagination - Object containing pagination options
+   *
+   * @example
+   * Retrieve all Custom Domains
+   * ```js
+   * const customDomains = plusAuth.customDomains.getAll()
+   * ```
+   *
+   * @example
+   * Retrieve first 5 Custom Domains
+   * ```js
+   * const customDomains = await plusAuth.customDomains.getAll({ itemsPerPage: 5 })
+   * ```
+   */
   async getAll( pagination: IPagination ): Promise<PaginatedResult<ICustomDomain>> {
     return this.http.get( encodedQueryString( pagination ) );
   }
 
+
+  /**
+   * Get Custom Domain with id
+   *
+   * @param customDomainId - Custom Domain id
+   *
+   * @example
+   * ```js
+   * const customDomain = await plusAuth.customDomains.get('CUSTOM_DOMAIN_ID')
+   * ```
+   */
   async get( customDomainId: string ): Promise<ICustomDomain> {
     return this.http.get( `/${ customDomainId }` );
   }
 
-  async create( props: any ): Promise<ICustomDomain> {
-    return this.http.post( '', props );
+
+  /**
+   * Create a custom domain entry
+   *
+   * @param customDomain - Object containing custom domain properties
+   *
+   * @example
+   * ```js
+   * const customDomain = await plusAuth.customDomains.create({ domain: 'myexamplesite.com' })
+   * ```
+   */
+  async create( customDomain: Omit<ICustomDomain, 'validated'> ): Promise<ICustomDomain> {
+    return this.http.post( '', customDomain );
   }
 
-  async update( customDomainId: string, props: any ): Promise<ICustomDomain> {
-    return this.http.patch( `/${ customDomainId }`, props );
-  }
-
+  /**
+   * Remove a custom domain entry
+   *
+   * @param customDomainId - Custom Domain Id to be removed
+   *
+   * @example
+   * ```js
+   * if( await plusAuth.customDomains.remove('CUSTOM_DOMAIN_ID') ){
+   *  console.log('custom domain removed')
+   * }
+   * ```
+   */
   async remove( customDomainId: string ): Promise<void> {
     return this.http.delete( `/${ customDomainId }` );
   }
 
+
+  /**
+   * Trigger validation of custom domain ownership
+   *
+   * @param customDomainId - Custom Domain Id to be validated
+   *
+   * @example
+   * ```js
+   * if( await plusAuth.customDomains.validate('CUSTOM_DOMAIN_ID') ){
+   *  console.log('custom domain ownership validated')
+   * }
+   * ```
+   */
   async validate( customDomainId: string ): Promise<void> {
     return this.http.get( `/${ customDomainId }/validate` );
   }
--- src/api/keys.ts
+++ src/api/keys.ts
@@ -1,31 +1,113 @@
 import { HttpService } from '../http';
 /**
- * Service for interacting with API's defined in PlusAuth
+ * Service for interacting with signing and encryption keys of your tenant in PlusAuth
  * @public
  */
 export class KeyService extends HttpService{
   protected static prefix = '/keys'
 
+  /**
+   * Retrieve signing keys of your tenant.
+   *
+   * @example
+   * ```js
+   * const signingKeys = await plusAuth.keys.getSigningKeys()
+   * ```
+   */
   async getSigningKeys( ): Promise<JsonWebKey[]> {
     return this.http.get( '/signing' );
   }
 
+  /**
+   * Rotate your signing keys. If keys are not provided,
+   * a new key will be auto generated for each key type and algorithm in current key set.
+   *
+   * @example
+   * ```js
+   * Let PlusAuth generate new keys
+   * if(await plusAuth.keys.rotateSigningKeys()){
+   *   console.log('signing keys rotated')
+   * }
+   * ```
+   *
+   * @example
+   * With your own key set
+   * ```js
+   * const keys = generateJWKS() // implement this according to your needs
+   * if(await plusAuth.keys.rotateSigningKeys(keys)){
+   *   console.log('signing keys rotated')
+   * }
+   * ```
+   */
   async rotateSigningKeys( keys?: JsonWebKey[] ): Promise<void> {
     return this.http.post( '/signing/rotate', { keys } );
   }
 
+  /**
+   * Revoke an existing signing key in your tenant's signing key set.
+   *
+   * @param kid - Key's id to be revoked
+   *
+   * @example
+   * ```js
+   * if(await plusAuth.keys.revokeSigningKey('KEY_ID')){
+   *   console.log('signing key revoked')
+   * }
+   * ```
+   */
   async revokeSigningKey( kid: string ): Promise<void> {
     return this.http.get( `/signing/revoke/${ kid }` );
   }
 
+  /**
+   * Retrieve encryption keys of your tenant.
+   *
+   * @example
+   * ```js
+   * const encryptionKeys = await plusAuth.keys.getEncryptionKeys()
+   * ```
+   */
   async getEncryptionKeys( ): Promise<JsonWebKey[]> {
     return this.http.get( '/encryption' );
   }
 
+  /**
+   * Rotate your encryption keys. If keys are not provided,
+   * a new key will be auto generated for each key type and algorithm in current key set.
+   *
+   * @example
+   * ```js
+   * Let PlusAuth generate new keys
+   * if(await plusAuth.keys.rotateEncryptionKeys()){
+   *   console.log('signing keys rotated')
+   * }
+   * ```
+   *
+   * @example
+   * With your own key set
+   * ```js
+   * const keys = generateJWKS() // implement this according to your needs
+   * if(await plusAuth.keys.rotateEncryptionKeys(keys)){
+   *   console.log('encryption keys rotated')
+   * }
+   * ```
+   */
   async rotateEncryptionKeys( keys?: JsonWebKey[] ): Promise<void> {
     return this.http.post( '/encryption/rotate', { keys } );
   }
 
+  /**
+   * Revoke an existing encryption key in your tenant's encryption key set.
+   *
+   * @param kid - Key's id to be revoked
+   *
+   * @example
+   * ```js
+   * if(await plusAuth.keys.revokeEncryptionKey('KEY_ID')){
+   *   console.log('encryption key revoked')
+   * }
+   * ```
+   */
   async revokeEncryptionKey( kid: string ): Promise<void> {
     return this.http.get( `/encryption/revoke/${ kid }` );
   }
