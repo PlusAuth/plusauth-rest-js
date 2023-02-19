96efadc4bf7ed99d6c842185bfecc27d67b413d4
Ismail H. Ayaz <ayazismailhakki@gmail.com>
2023-02-19 22:39:10 +0300

    fix: incorrect uri query encoding

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,7 +5,7 @@ export function encodedQueryString( data?: { [key: string]: any }, appendable = 
   if ( !data ) return '';
   const ret: string[] = [];
   for ( const d in data ) {
-    if ( data[d] != null ) ret.push( `${ encodeURIComponent( d ) }=${ data[d].toString() }` );
+    if ( data[d] != null ) ret.push( `${ d }=${ encodeURIComponent( data[d].toString() ) }` );
   }
   if ( appendable ) {
     return `?${ ret.join( '&' ) }`;
